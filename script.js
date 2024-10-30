94b921a681f407e84e39043d1981a4cedb39284d
webdev3186 <webdev3186@gmail.com>
2024-10-30 13:16:35 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-// script.js
 document.addEventListener("DOMContentLoaded", function () {
-    // Load Ad Code
-    fetch('author-bio.html')
-        .then(response => response.text())
+    fetch('./author-bio.html')
+        .then(response => {
+            if (!response.ok) throw new Error('Network response was not ok.');
+            return response.text();
+        })
         .then(data => {
             document.getElementById('ads').innerHTML += data;
-        });
+        })
+        .catch(error => console.error('Error loading ads:', error));
 });
